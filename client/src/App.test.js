4c270396696b864ba8f8d75812369663473da9da
umer2k200 <umarzeeshan709@gmail.com>
2024-11-25 18:40:58 +0500

    fourth

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Simple MERN App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Simple MERN App/i);
   expect(linkElement).toBeInTheDocument();
 });
